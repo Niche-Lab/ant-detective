a57939deaceb8d0e4c0e46ee1192a006473e8954
poissonfish <niche@vt.edu>
2023-02-10 13:29:41 -0500

    update ViT

--- models/ViT.py
+++ models/ViT.py
@@ -12,25 +12,26 @@ class Niche_ViT(Niche_Model):
         super(Niche_ViT, self).__init__()
         weights = ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1
         self.model = vit_b_16(weights=weights)
-        self.model.heads = nn.Linear(self.model.heads.head.in_features, 512)
-        self.mlps = nn.Sequential(
+        self.model.heads = nn.Sequential(
+            # 1st layer
+            nn.Linear(self.model.heads.head.in_features, 512),
             nn.GELU(),
             nn.BatchNorm1d(512),
             nn.Dropout(0.01),
+            # 2nd layer
             nn.Linear(512, 512),
-            nn.BatchNorm1d(512),
             nn.GELU(),
+            nn.BatchNorm1d(512),
             nn.Dropout(0.01),
+            # 3rd layer
             nn.Linear(512, 128),
             nn.BatchNorm1d(128),
             nn.GELU(),
             nn.Dropout(0.01),
+            # 4th layer
             nn.Linear(128, 2),
-            nn.GELU(),
             nn.BatchNorm1d(2),
         )
 
     def forward(self, x):
-        x = self.model(x)
-        x = self.mlps(x)
-        return x
+        return self.model(x)
