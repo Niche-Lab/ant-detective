847cc91cfe6bb6a0a743e5f1127fadac7334eb62
poissonfish <niche@vt.edu>
2025-05-28 02:30:29 -0400

    comment out example command in study1 script

--- _2_study1.sh
+++ _2_study1.sh
@@ -35,10 +35,10 @@ for ITER in {1..100}; do
 done
 
 
-python study1.py\
-    --iters 0\
-    --thread 1\
-    --n_samples 64\
-    --modelname yolo11n\
-    --test
+# python study1.py\
+#     --iters 0\
+#     --thread 1\
+#     --n_samples 64\
+#     --modelname yolo11n\
+#     --test
 
